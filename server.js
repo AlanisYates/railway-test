f14a43a14533412443d9c0ee00e3e8efad99ae9e
Alanis Yates <yatesalanis@gmail.com>
2022-11-04 14:27:20 -0400

    gql server

--- server.js
+++ server.js
@@ -1,13 +1,40 @@
 import express from "express";
+import { ApolloServer, gql } from "apollo-server-express";
 
-const app = express();
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
 
-const port = process.env.PORT || 3000;
+const resolvers = {
+  Query: {
+    hello: () => "Hello World",
+  },
+};
 
-app.get("/", (req, res) => {
-  res.send("Hello world from alanis");
-});
+// const server = new ApolloServer({ typeDefs, resolvers });
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}....`);
-});
+// const port = process.env.PORT || 3000;
+
+// app.get("/", (req, res) => {
+//   res.send("Hello world");
+// });
+
+// app.listen(port, () => {
+//   console.log(`App running on port ${port}....`);
+// });
+
+async function startServer() {
+  const app = express();
+  const server = new ApolloServer({ typeDefs, resolvers });
+
+  await server.start();
+  server.applyMiddleware({ app });
+
+  app.listen({ port: 4000 }, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  );
+}
+
+startServer();
